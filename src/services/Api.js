8614674fae390b5ba1de7c45898ae5f996d0d4b5
Stuart Thompson <stuart.thompson@nytimes.com>
2018-09-04 21:02:17 -0400

    S

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: 'https://sheltered-earth-60660.herokuapp.com/'
+    baseURL: 'http://sheltered-earth-60660.herokuapp.com/'
     // baseURL: `http://localhost:8081`
   })
 }
